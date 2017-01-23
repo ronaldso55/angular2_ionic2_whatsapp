9814b99ecc72623b20cd9e89d5e9a3c4bef08734
Ronald So <ronald.so@icloud.com>
2017-01-22 23:33:11 -0500

    Step 2: Chat & Msg model, moment package, hard coded data with SCSS.

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -2,12 +2,16 @@ import { Component } from '@angular/core';
 import { Platform } from "ionic-angular";
 import { StatusBar } from "ionic-native";
 import template from './app.component.html';
+import { TabsContainerComponent } from "../pages/tabs-container/tabs-container.component";
+import { ChatsComponent } from "../pages/chats/chats.component";
 
 @Component({
   selector: 'app',
   template
 })
 export class AppComponent {
+  rootPage = TabsContainerComponent;
+
   constructor(platform: Platform) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
--- client/imports/app/app.module.ts
+++ client/imports/app/app.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { IonicApp, IonicModule } from "ionic-angular";
+import { TabsContainerComponent } from "../pages/tabs-container/tabs-container.component";
+import { ChatsComponent } from "../pages/chats/chats.component";
+import { MomentModule } from "angular2-moment";
 
 @NgModule({
   // Components, Pipes, Directive
   declarations: [
-    AppComponent
+    AppComponent,
+    TabsContainerComponent,
+    ChatsComponent
   ],
   // Entry Components
   entryComponents: [
-    AppComponent
+    AppComponent,
+    TabsContainerComponent,
+    ChatsComponent
   ],
   // Providers
   providers: [
@@ -17,7 +24,8 @@ import { IonicApp, IonicModule } from "ionic-angular";
   ],
   // Modules
   imports: [
-    IonicModule.forRoot(AppComponent)
+    IonicModule.forRoot(AppComponent),
+    MomentModule
   ],
   // Main Component
   bootstrap: [ IonicApp ]
